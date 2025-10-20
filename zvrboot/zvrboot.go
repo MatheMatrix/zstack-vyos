0c8fbac03b07d371c1a362ef32557073bed26d72
shixin.ruan <shixin.ruan@zstack.io>
2025-10-20 13:53:48 +0800

    Revert "Merge branch 'shixin-ZSTAC-79083' into '5.3.40-SeresSnat'"
    This reverts merge request !1021

--- zvrboot/zvrboot.go
+++ zvrboot/zvrboot.go
@@ -492,8 +492,6 @@ func configureVyos() {
 			}
 			if nic.category == "Private" {
 				err = utils.InitNicFirewall(nic.name, nic.ip, false, utils.IPTABLES_ACTION_REJECT)
-				// add private SNAT rule in iptables
-				err = utils.AddSnatRuleForPrivateNic(nic.name, nic.ip, nic.netmask)
 			} else {
 				err = utils.InitNicFirewall(nic.name, nic.ip, true, utils.IPTABLES_ACTION_REJECT)
 			}
@@ -554,19 +552,6 @@ func configureVyos() {
 
 			setNicTree.AttachFirewallToInterface(nic.name, "local")
 			setNicTree.AttachFirewallToInterface(nic.name, "in")
-
-			if nic.category == "Private" {
-				// add private SNAT rule in VyOS config tree
-				nicNo, _ := utils.GetNicNumber(nic.name)
-				ruleNo := utils.GetPrivateNicSNATRuleNumber(nicNo)
-				address, _ := utils.GetNetworkNumber(nic.ip, nic.netmask)
-				setNicTree.SetSnatWithRuleNumber(ruleNo,
-					fmt.Sprintf("outbound-interface %s", nic.name),
-					fmt.Sprintf("source address %s", address),
-					"destination address !224.0.0.0/8",
-					fmt.Sprintf("translation address %s", nic.ip),
-				)
-			}
 		}
 	}
 
