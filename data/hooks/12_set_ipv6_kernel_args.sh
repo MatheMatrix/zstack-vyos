b3bfc1af6afbc5b04860e5854e214a05d10ba624
pengchao.liu <pengchao.liu@zstack.io>
2026-08-28 11:30:07 +0800

    <fix>[vrouter]: allow hotplug NAT routing
    openEuler's systemd-sysctl reapplies strict reverse-path filtering
    when udev discovers a new NIC, overriding the VRouter default after
    attach.
    
    Use one later sysctl.d policy for boot, upgrades, and hotplug so
    existing and new interfaces converge to the same value.
    
    Resolves: ZSTAC-87740
    Change-Id: Idfaec9b2345f513322d85a35ff753271b3412113

--- data/hooks/12_set_ipv6_kernel_args.sh
+++ data/hooks/12_set_ipv6_kernel_args.sh
@@ -3,13 +3,15 @@
 # set all,default value
 sysctl -p
 
+VROUTER_SYSCTL_FILE=/etc/sysctl.d/99-zstack-vrouter.conf
+SYSTEMD_SYSCTL=/usr/lib/systemd/systemd-sysctl
+if [ -f "$VROUTER_SYSCTL_FILE" ] && [ -x "$SYSTEMD_SYSCTL" ]; then
+  "$SYSTEMD_SYSCTL" "$VROUTER_SYSCTL_FILE"
+fi
+
 # set specific nic value
 NIC_NAMES=$(ls /sys/class/net)
 for nic in $NIC_NAMES; do
-  rp_filter_path="/proc/sys/net/ipv4/conf/$nic/rp_filter"
-  if [ -e "$rp_filter_path" ]; then
-    echo 0 > "$rp_filter_path"
-  fi
   echo 0 > /proc/sys/net/ipv6/conf/$nic/accept_dad
   echo 1 > /proc/sys/net/ipv6/conf/$nic/keep_addr_on_down
 done
